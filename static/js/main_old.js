d0286e366eab455d7f09f0f9ab0cff273862cbc0
juamngie <82877810+juamngie@users.noreply.github.com>
2023-04-17 17:00:09 +0800

    Add files via upload

--- static/js/main_old.js
+++ static/js/main_old.js
@@ -26,5 +26,32 @@ $(document).ready(function () {
         readURL(this);
     });
 
-    
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('.image-exp').show();
+                $('#result').fadeIn(600);
+                $('#result').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
+
 });
